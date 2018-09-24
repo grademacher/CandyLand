13068a91b2cce65051f10f0ae9ea7acadfbad47b
grademacher <rademacher.garrett@gmail.com>
2018-09-24 15:44:19 -0400

    suffling deck now works

--- Project1/deck.cpp
+++ Project1/deck.cpp
@@ -15,23 +15,23 @@ Deck::Deck(std::vector<int>* deck_layout)
 
   //print out the deck
   for (int i = 0; i < my_deck->size(); i++) {
-    std::cout << my_deck->at(i);
+    std::cout << my_deck->at(i) << " ";
   }
 
   //do an in place shuffle on the deck
   for (int i = 0; i < my_deck->size() - 2; i++) {
     std::random_device rd; // obtain a random number from hardware
     std::mt19937 eng(rd()); // seed the generator
-    std::uniform_int_distribution<> distr(i, my_deck->size()+1); // define the range
+    std::uniform_int_distribution<> distr(i, my_deck->size()-1); // define the range
 
-    for (int n = 0; n<40; ++n)
-      std::cout << distr(eng) << ' '; // generate numbers
+    std::swap(my_deck->at(i),my_deck->at(distr(eng)));
 
   }
 
   //print out the deck again
+  std::cout << "\n\n\n";
   for (int i = 0; i < my_deck->size(); i++) {
-    std::cout << my_deck->at(i);
+    std::cout << my_deck->at(i) << " ";
   }
 }
 
